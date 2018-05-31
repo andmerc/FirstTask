3d53b583576487bb7259af5398229530af571e92
andmerc <32245832+andmerc@users.noreply.github.com>
2018-05-31 18:29:08 +0300

    Adding Task 2
    The story about Mary and James, who wanted to buy expensive things

---
+++ Task 2/Task 2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Task_2
+{
+    class Program
+    {
+        static object dollars250 = new object();
+        static object dollars350 = new object();
+
+        static void BuyingEarrings()
+        {
+            lock (dollars250)
+            {
+                Thread.Sleep(500);
+                lock (dollars350)
+                {
+                    Console.WriteLine("Mary bought earrings");
+                }
+            }
+        }
+
+        static void BuyingWatches()
+        {
+            lock (dollars350)
+            {
+                Thread.Sleep(500);
+                lock (dollars250)
+                {
+                    Console.WriteLine("James bought watches");
+                }
+            }
+        }
+
+        static void Main()
+        {
+            var James = new Thread(BuyingWatches);
+            var Mary = new Thread(BuyingEarrings);
+
+            James.Start();
+            Mary.Start();
+
+            Console.WriteLine("Main thread ended");
+        }
+    }
+}
